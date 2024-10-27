09292eb5cba08b1892d7cd331d0c41b83ef29d7c
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-10-27 13:57:56 +0300

    create base configs and install dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "@nuxtjs/google-fonts",
+    "dayjs-nuxt",
+  ],
+  srcDir: "src",
+  dir: {
+    public: "../public",
+    layouts: "app/layouts",
+  },
+  tailwindcss: {
+    viewer: false,
+    disableHMR: true,
+    quiet: true,
+    cssPath: ["~/app/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+  },
+  googleFonts: {},
+  vite: {
+    server: {
+      hmr: false,
+    },
+  },
+});
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-3xl text-red-400">hello</h1>
+</template>
